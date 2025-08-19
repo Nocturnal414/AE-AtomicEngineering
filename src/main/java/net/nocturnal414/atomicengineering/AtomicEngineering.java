897b04f8f0f5c58397f4bcfc903a16f3da82c5dc
Nocturnal414 <nocturnal.minegames@gmail.com>
2025-08-20 02:28:55 +0700

    Pre Radiation Engine Prep

--- src/main/java/net/nocturnal414/atomicengineering/AtomicEngineering.java
+++ src/main/java/net/nocturnal414/atomicengineering/AtomicEngineering.java
@@ -1,15 +1,12 @@
 package net.nocturnal414.atomicengineering;
 
 import com.mojang.logging.LogUtils;
-import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.nocturnal414.atomicengineering.block.ModBlocks;
@@ -55,12 +52,4 @@ public class AtomicEngineering {
 
     }
 
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-    public static class ClientModEvents {
-        @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event) {
-
-        }
-    }
 }
